f5717ef840a762f65e27deadbc58d385fc910173
Antonio Stiube <101660252+NoMercy17@users.noreply.github.com>
2026-09-20 11:16:16 -0700

    Create Armor class for item representation
    Added Armor class to represent armor items with defense.

---
+++ inventory-system/headers/Armor.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "Item.hpp"
+
+class Armor : public Item {
+public:
+	Armor(std::string name, Rarity rarity, double weight, int price, double defense);
+
+	void use(Character& target) override; // we equip and reduce dmg from the target
+	std::string describe() const override;
+	ItemType type() override { return ItemType::Armor; }
+
+	double getDefense() const { return defense_; }
+
+protected:
+	double defense_;
+};
